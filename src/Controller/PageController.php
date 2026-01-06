47206df7e344995c8cf672d8c9fe82dd1cc4cdf1
Yanis <yanis.laid@etu.univ-st-etienne.fr>
2026-01-06 10:25:30 +0100

    Intégration du controlleur inscription directement dans le controlleur principal

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -3,8 +3,12 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request; // Nécessaire pour le formulaire
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class PageController extends AbstractController
 {
@@ -30,5 +34,42 @@ class PageController extends AbstractController
     public function loisirs(): Response
     {
         return $this->render('loisirs/index.html.twig');
+    }
+    
+    #[Route('/inscription', name: 'app_inscription')]
+    public function inscription(Request $request): Response
+    {
+        $form = $this->createFormBuilder(null)
+            ->add('nom', TextType::class, [
+                'label' => 'Votre Nom',
+                'attr' => ['class' => 'form-control', 'placeholder' => 'Ex: Dupont']
+            ])
+            ->add('prenom', TextType::class, [
+                'label' => 'Votre Prénom',
+                'attr' => ['class' => 'form-control', 'placeholder' => 'Ex: Jean']
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Votre Email',
+                'attr' => ['class' => 'form-control', 'placeholder' => 'Ex: contact@email.com']
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Accéder au CV',
+                'attr' => ['class' => 'btn btn-primary mt-3 w-100']
+            ])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            return $this->render('inscription/success.html.twig', [
+                'nom' => $data['nom'],
+                'prenom' => $data['prenom']
+            ]);
+        }
+
+        return $this->render('inscription/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
